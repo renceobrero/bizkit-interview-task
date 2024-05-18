a31eb98e377e8eb71205d3938c99b536627a562a
renceobrero <renceobrero@gmail.com>
2024-05-18 11:37:47 +0800

    Implement match, search, and bonus challenge

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,15 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
+    """Check if all numbers in fave_numbers_2 are present in fave_numbers_1.
 
-    return True
+    Parameters:
+        fave_numbers_1 (iterable): The first set of favorite numbers.
+        fave_numbers_2 (iterable): The second set of favorite numbers.
+
+    Returns:
+        a bool where True if all numbers in fave_numbers_2 are present in fave_numbers_1, else False.
+    """
+
+    fave_numbers_1_set = set (fave_numbers_1)
+    return all(number in fave_numbers_1_set for number in fave_numbers_2)
--- phasebook/search.py
+++ phasebook/search.py
@@ -27,4 +27,53 @@ def search_users(args):
 
     # Implement search here!
 
-    return USERS
+    if (args): 
+        matched_users = []
+        unique_ids = set()
+
+        for user in USERS:
+            match_weight = calculate_match_weight(user, args)
+
+            if match_weight and user["id"] not in unique_ids:
+                matched_users.append((user, match_weight))
+                unique_ids.add(user["id"])
+    
+        matched_users.sort(key=lambda user: user[1], reverse=True)
+
+        return [user_info[0] for user_info in matched_users]
+    else:
+        return USERS
+
+
+def calculate_match_weight(user, search_params): 
+    """Calculate the match weight for a user based on search parameters.
+
+    Parameters:
+        user (dict): a dictionary representing a user with keys:
+            id: string
+            name: string
+            age: string
+            occupation: string
+        
+        search_params (dict): a dictionary containing the following parameters:
+            id: string
+            name: string
+            age: string
+            occupation: string
+
+    Returns:
+        an int that represents the match weight for the user, where higher weights indicate high priority matches.
+    """
+
+    match_priority = {"id": 4, "name": 3, "age": 2, "occupation": 1}
+    
+    if "id" in search_params and str(user["id"]) == search_params["id"]:
+        return match_priority["id"]
+    elif "name" in search_params and search_params["name"].lower() in user["name"].lower():
+        return match_priority["name"]
+    elif "age" in search_params and user["age"] in range(int(search_params["age"]) - 1, int(search_params["age"]) + 2):
+        return match_priority["age"]
+    elif "occupation" in search_params and search_params["occupation"].lower() in user["occupation"].lower():
+        return match_priority["occupation"]
+    else:
+        return 0
